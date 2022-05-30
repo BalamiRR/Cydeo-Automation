d81a91545b92dd5cd486e0b154d551792d01e866
Fuat Kara -                                 Junior Java Developer <36539306+BalamiRR@users.noreply.github.com>
2022-05-30 14:34:59 +0200

    Selenium

---
+++ src/test/java/com/fuatkara/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
@@ -0,0 +1,10 @@
+package com.fuatkara.tests.day5_testNG_intro_dropdowns;
+
+import org.testng.annotations.Test;
+
+public class TestNG_Intro {
+    @Test
+    public  void test1(){
+        System.out.println("TEST 1 is running!!");
+    }
+}
