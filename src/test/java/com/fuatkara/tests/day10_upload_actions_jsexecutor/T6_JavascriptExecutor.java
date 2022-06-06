f29d3c55be6450a403500dd05dd61c5475133812
Fuat Kara -                                 Junior Java Developer <36539306+BalamiRR@users.noreply.github.com>
2022-06-06 21:35:12 +0200

    Selenium

---
+++ src/test/java/com/fuatkara/tests/day10_upload_actions_jsexecutor/T6_JavascriptExecutor.java
@@ -0,0 +1,36 @@
+package com.fuatkara.tests.day10_upload_actions_jsexecutor;
+
+import com.fuatkara.utilities.Driver;
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.annotations.Test;
+
+public class T6_JavascriptExecutor {
+
+    @Test
+    public void javascript_executor_test(){
+        //TC #6: Scroll using JavascriptExecutor
+        //1- Open a chrome browser
+        //2- Go to: https://practice.cydeo.com/infinite_scroll
+        Driver.getDriver().get("https://practice.cydeo.com/infinite_scroll");
+
+        //((JavascriptExecutor)Driver.getDriver()).executeScript();
+        //3- Use below JavaScript method and scroll*********** when we scroll it continues
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+
+        //a. 750 pixels down 10 times.
+//        for (int i = 0; i < 10; i++) {
+//            //BrowserUtils.sleep();
+//            js.executeScript("window.scrollBy(0, 750)");
+//        }
+        js.executeScript("window.scrollBy(0, 750)");
+
+
+        //b. 750 pixels up 10 times
+//        for (int i = 0; i < 10; i++) {
+//            //BrowserUtils.sleep();
+//            js.executeScript("window.scrollBy(0, -750)");
+//        }
+
+        //JavaScript method to use : window.scrollBy(0,0)
+    }
+}
